389bf0b3d5a1c7b8e151a3953e18b208760954b1
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2024-12-23 20:55:57 +0530

    deps: Added Httpoison

--- mix.exs
+++ mix.exs
@@ -39,7 +39,8 @@ defmodule HuggingfaceInference.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.28", only: :dev, runtime: false},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:httpoison, "~> 2.2"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]

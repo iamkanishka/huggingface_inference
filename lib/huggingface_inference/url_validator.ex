8fd7a3461df93139a8b30d0e60f42ca0bc0c552f
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2024-12-23 20:57:13 +0530

    feat: UrlValidator module

---
+++ lib/huggingface_inference/url_validator.ex
@@ -0,0 +1,41 @@
+defmodule HuggingfaceInference.UrlValidator do
+  @moduledoc """
+  A utility module for validating if a given string is a URL.
+
+  Supports checking both absolute (`http`, `https`) and relative URLs (starting with `/`).
+  """
+
+  @doc """
+  Checks if the given string is a URL.
+
+  ## Parameters
+    - `model_or_url` (String.t): The string to validate.
+
+  ## Returns
+    - `true` if the string is a valid URL.
+    - `false` otherwise.
+
+  ## Examples
+
+      iex> UrlValidator.is_url("https://example.com")
+      true
+
+      iex> UrlValidator.is_url("/relative/path")
+      true
+
+      iex> UrlValidator.is_url("not-a-url")
+      false
+
+  ## Errors
+
+  Raises `ArgumentError` if the input is `nil` or not a string.
+  """
+  @spec is_url(String.t()) :: boolean()
+  def is_url(nil), do: raise(ArgumentError, "Input cannot be nil")
+
+  def is_url(model_or_url) when is_binary(model_or_url) do
+    Regex.match?(~r/^http(s?):/, model_or_url) or String.starts_with?(model_or_url, "/")
+  end
+
+  def is_url(_), do: raise(ArgumentError, "Input must be a string")
+end
